626a61d4027bd8b80296beb830405589b44c625f
Ludovic YOL <ludovic.yol@gmail.com>
2018-12-17 22:20:25 +0100

    update user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,26 +1,33 @@
 import User from "../models/UserModel";
 
-module.exports.findById = function(req, res) {
-    User
-      .findById(req.params.id)
-      .exec(function(err, user) {
-        res.status(200).json(user);
-      });
-};
-
-module.exports.update = function(req, res) {
+module.exports.findById = function (req, res) {
   User
-    .update(req.body)
-    .exec(function(err, user) {
+    .findById(req.params.id)
+    .exec(function (err, user) {
       res.status(200).json(user);
     });
 };
 
-module.exports.findAll = function(req, res) {
-  User.find((err, issues) => {
-      if (err)
+module.exports.update = function (req, res) {
+  console.log(req.body);
+  User
+    .findByIdAndUpdate(
+      req.body._id,
+      req.body, {
+        new: true
+      },
+      (err, user) => {
+        if (err)
           console.log(err);
-      else
-          res.json(issues);
+        res.status(200).json(user);
+      });
+};
+
+module.exports.findAll = function (req, res) {
+  User.find((err, issues) => {
+    if (err)
+      console.log(err);
+    else
+      res.json(issues);
   });
 };
