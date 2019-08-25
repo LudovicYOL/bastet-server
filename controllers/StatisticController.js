54ec4ae4ab64533ecef51a746647a079da7b28d2
Ludovic YOL <ludovic.yol@gmail.com>
2019-08-25 23:26:46 +0200

    sorting promotion stat results

--- controllers/StatisticController.js
+++ controllers/StatisticController.js
@@ -2,7 +2,10 @@ import User from "../models/UserModel";
 
 module.exports.promotion = function (req, res) {
     User.aggregate(
-        [{ $group: { _id: "$promotion", count: { $sum: 1 } } }],
+        [
+            { $group: { _id: "$promotion", count: { $sum: 1 } } },
+            { $sort: { "_id": 1 } }
+        ],
         function (err, result) {
             if (err) return handleError(err);
             res.status(200).json(result);
