aac534688af1a224407de305f9f8c5cd9d20cc3b
Ludovic YOL <ludovic.yol@gmail.com>
2019-08-25 23:46:41 +0200

    adding update password endpoint

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,8 +1,8 @@
 import Account from "../models/AccountModel";
 
 module.exports.updateLogin = function (req, res) {
-    // Password verification
     Account.findOne({ _id: req.body.id }, function (err, account) {
+        // Password verification
         if (!account.validPassword(req.body.password)) {
             res.status(400).json({"error":"Wrong password"});
         } else { 
@@ -17,15 +17,18 @@ module.exports.updateLogin = function (req, res) {
 };
 
 module.exports.updatePassword = function (req, res) {
-    let account = Account.findById(req.body._id);
-    account.setPassword(req.body.password);
-    account.save()
-        .then(mission => {
-            res.status(200).json(mission);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+    Account.findOne({ _id: req.body.id }, function (err, account) {
+         // Password verification
+        if (!account.validPassword(req.body.actualPassword)) {
+            res.status(400).json({"error":"Wrong password"});
+        } else { 
+             // If ok, update password
+            account.setPassword(req.body.newPassword);
+            account.save(function(err, response) {
+                res.status(200).json(response);
+            });
+        }
+    });
 };
 
 
